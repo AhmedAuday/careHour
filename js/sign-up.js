dd2b827412cacbd4332e39a41b5ef98a60eb6934
Zhyo21 <zhyarunivar@gmail.com>
2023-05-15 12:50:51 +0300

    test

--- js/sign-up.js
+++
@@ -1,8 +0,0 @@
-function next(){
-const changeClassBtn = document.getElementsByClassName('button');
-const navLink = document.getElementById('nav-link');
-alert("hosh");
-changeClassBtn.addEventListener('click', () => {
-  
-  navLink.classList.toggle('active');
-})};
